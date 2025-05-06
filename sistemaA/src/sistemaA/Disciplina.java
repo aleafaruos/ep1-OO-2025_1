ba17a313e7b1e84b6886f2173ec31e65a47ba05a
Rafaela <rafaela0118@gmail.com>
2025-05-06 14:47:16 -0300

    criação da classe Disciplina.java

---
+++ sistemaA/src/sistemaA/Disciplina.java
@@ -0,0 +1,48 @@
+package sistemaA;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Disciplina {
+    String nome;
+    String codigo;
+    int cargaHoraria;
+    List<String> preRequisitos;
+
+
+    public Disciplina(String nome, String codigo, int cargaHoraria,List<String> preRequisitos){
+        this.nome="";
+        this.codigo="";
+        this.cargaHoraria=0;
+        this.preRequisitos=new ArrayList<String>();
+}
+
+    public String getNome(){
+        return nome;
+}
+
+    public String getCodigo(){
+        return codigo;
+}
+
+    public int getCargaHoraria(){
+        return cargaHoraria;
+}
+
+    public List<String> getpreRequisitos(){
+        return preRequisitos;
+
+}
+
+    public void adicionarPreRequisito(String disciplina){
+        preRequisitos.add(disciplina);
+    }
+
+    public void removerPreRequisitos(String disciplina){
+        preRequisitos.remove(disciplina);
+    }
+
+    public boolean verificarPreRequisitos(List<String> disciplinasMatriculadas) {
+        return disciplinasMatriculadas.containsAll(preRequisitos);
+    }
+    
+}
