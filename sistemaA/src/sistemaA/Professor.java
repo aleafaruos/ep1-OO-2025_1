0354fcdcd2da701fea684015a6b465857a685cf6
Rafaela <rafaela0118@gmail.com>
2025-05-19 16:52:39 -0300

    adicionando atributos a professor.java: nome, CPF, email e lista de turma e métodos para adicionar turmas, listar turmas.

--- sistemaA/src/sistemaA/Professor.java
+++ sistemaA/src/sistemaA/Professor.java
@@ -1,5 +1,52 @@
-package io.github.jiangdequan;
+package sistemaA;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Professor {
-    
+    private String nome;
+    private String cpf;
+    private String email;
+    private List<Turma> turmas;
+
+    public Professor(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.turmas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Turma> getTurmas() {
+        return turmas;
+    }
+
+    public void adicionarTurma(Turma turma) {
+        if (!turmas.contains(turma)) {
+            turmas.add(turma);
+        }
+    }
+
+    public void listarTurmas() {
+        System.out.println("Turmas do professor " + nome + ":");
+        for (Turma turma : turmas) {
+            System.out.println("- " + turma.getCodigo() + " (" + turma.getDisciplina().getNome() + ")");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Professor: " + nome + ", CPF: " + cpf + ", Email: " + email;
+    }
 }
